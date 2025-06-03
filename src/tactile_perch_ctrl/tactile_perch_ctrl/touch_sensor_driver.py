51ffead175e8d3b8615d6f3fd00d0a837a3932a7
antbre <a.bredenbeck@tudelft.nl>
2025-06-03 10:38:34 +0200

    Fix timestamp typo

--- src/tactile_perch_ctrl/tactile_perch_ctrl/touch_sensor_driver.py
+++ src/tactile_perch_ctrl/tactile_perch_ctrl/touch_sensor_driver.py
@@ -33,7 +33,7 @@ class TouchSensorDriver(Node):
     
     def timer_callback(self):
         msg = TouchData()
-        msg.header.timestamp = self.get_clock().now().to_msg()
+        msg.header.stamp = self.get_clock().now().to_msg()
         
         msg.raw_data = self.check_for_raw_data()
         msg.filtered_data = self.check_for_filtered_data()
