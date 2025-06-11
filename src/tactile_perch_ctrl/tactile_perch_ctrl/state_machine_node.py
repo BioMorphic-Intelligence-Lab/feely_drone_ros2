402bf65e2941bc408d3e4312eb25952ed016ecc8
antbre <a.bredenbeck@tudelft.nl>
2025-06-11 17:25:43 +0200

    Add sm state publisher+fix contact data subscriber

--- src/tactile_perch_ctrl/tactile_perch_ctrl/state_machine_node.py
+++ src/tactile_perch_ctrl/tactile_perch_ctrl/state_machine_node.py
@@ -6,7 +6,7 @@ from rclpy.qos import qos_profile_sensor_data
 
 from geometry_msgs.msg import PoseStamped
 from sensor_msgs.msg import JointState
-from custom_msgs.msg import TouchData
+from custom_msgs.msg import TouchData, StateMachineState
 
 from .state_machine import StateMachine
 
@@ -17,7 +17,7 @@ class StateMachineNode(Node):
 
         # Declare all parameters
         self.declare_parameter("frequency", 20.0)
-        self.declare_parameter("touch_window_size", 125) # This assumes touch data is published at 250Hz, so 125 samples corresponds to 0.5 seconds
+        self.declare_parameter("touch_window_size", 10) # This assumes touch data is published at 250Hz, so 10 samples corresponds to 0.025 seconds
         self.declare_parameter("touch_threshold", 100)
         # Get all parameters
         self.frequency = self.get_parameter("frequency").get_parameter_value().double_value
@@ -31,6 +31,7 @@ class StateMachineNode(Node):
         self._ref_pos_publisher = self.create_publisher(PoseStamped, '/feely_drone/ref_pose', qos_profile_sensor_data)
         self._ref_joint_state_publisher = self.create_publisher(JointState, '/feely_drone/ref_joint_state', qos_profile_sensor_data)
         self._bin_touch_data_publisher = self.create_publisher(JointState, '/feely_drone/bin_touch_state', qos_profile_sensor_data)
+        self._sm_State_publisher = self.create_publisher(StateMachineState, '/feely_drone/state_machine_state', qos_profile_sensor_data)
         # Subscribers
         self._touch_data_subscriber = self.create_subscription(TouchData, '/feely_drone/touch_data', self.touch_data_callback, qos_profile_sensor_data)
 
@@ -48,13 +49,22 @@ class StateMachineNode(Node):
                                g=np.array([0, 0, -9.81]),      # Gravity Vector
                                target_pos_estimate=np.array([0, 0, 1.5]),
                                target_yaw_estimate=np.zeros([1]))
+        
+        # Init the binary touch state
+        self._bin_touch_state = np.zeros(9, dtype=bool)
 
     def timer_callback(self):
         # Get the reference pose and joint state messages
         pose_msg, joint_state_msg = self.get_references()
+
+        # Get the current state of the state machine
+        sm_state_msg = StateMachineState()
+        sm_state_msg.state = self.sm.state.value
+        sm_state_msg.header.stamp = pose_msg.header.stamp
         # Publish the reference pose and joint state messages
         self._ref_pos_publisher.publish(pose_msg)
         self._ref_joint_state_publisher.publish(joint_state_msg)
+        self._sm_State_publisher.publish(sm_state_msg)
 
     def get_references(self):
         # Get Timestamp
@@ -62,20 +72,20 @@ class StateMachineNode(Node):
 
         # Init publish messages
         pose = PoseStamped()
-        jointReference = JointState()
-        binTouchState = JointState()
+        jointReferenceMsg = JointState()
+        binTouchStateMsg = JointState()
 
         # Set timestamps and frame ids
         pose.header.stamp = timestamp_now
         pose.header.frame_id = 'world'
-        jointReference.header.stamp = timestamp_now
-        jointReference.name = [f'arm{i+1}' for i in range(3)]
-        binTouchState.header.stamp = timestamp_now
+        jointReferenceMsg.header.stamp = timestamp_now
+        jointReferenceMsg.name = [f'arm{i+1}' for i in range(3)]
+        binTouchStateMsg.header.stamp = timestamp_now
         
         # Compute the current touch state and publish for debugging purposes
-        touch_state = np.mean(self.touch_data_deque, axis=0) > self.TOUCH_THRESHOLD
-        binTouchState.position = touch_state.astype(float)
-        self._bin_touch_data_publisher.publish(binTouchState)
+        self._bin_touch_state = np.mean(self.touch_data_deque, axis=0) > self.TOUCH_THRESHOLD
+        binTouchStateMsg.position = self._bin_touch_state.astype(float)
+        self._bin_touch_data_publisher.publish(binTouchStateMsg)
         
         # Compute reference pose and arm opening angle
         control = self.sm.control(x=np.zeros(4),
@@ -94,13 +104,14 @@ class StateMachineNode(Node):
         pose.pose.orientation.z = np.sin(control['yaw_des'][0] / 2.0)
 
         # Extract Desired Arm Opening Angle
-        jointReference.position = control['alpha']
+        jointReferenceMsg.position = control['alpha']
 
-        return pose, jointReference
+        return pose, jointReferenceMsg
 
     def touch_data_callback(self, msg: TouchData):
         self.touch_data_deque.popleft()
         self.touch_data_deque.append(TouchData.raw_data)
+        self.sm.update_tactile_info_sw()
 
 def main(args=None):
     rclpy.init(args=args)
