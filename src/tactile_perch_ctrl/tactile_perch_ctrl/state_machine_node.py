daf5048f0618ecd1cac56eb99d3fc0d5bfab8a2f
antbre <a.bredenbeck@tudelft.nl>
2025-06-12 17:31:52 +0200

    Tune params of arm and add marker vis

--- src/tactile_perch_ctrl/tactile_perch_ctrl/state_machine_node.py
+++ src/tactile_perch_ctrl/tactile_perch_ctrl/state_machine_node.py
@@ -1,11 +1,13 @@
 import rclpy
 import numpy as np
+from scipy.spatial.transform import Rotation as R
 from collections import deque
 from rclpy.node import Node
-from rclpy.qos import qos_profile_sensor_data
+from rclpy.qos import qos_profile_sensor_data, qos_profile_default
 
 from geometry_msgs.msg import PoseStamped
 from sensor_msgs.msg import JointState
+from visualization_msgs.msg import Marker, MarkerArray
 from custom_msgs.msg import TouchData, StateMachineState
 
 from .state_machine import StateMachine
@@ -28,30 +30,49 @@ class StateMachineNode(Node):
         self.touch_data_deque = deque(np.zeros([self.touch_window_size, 12]))
        
         # Publishers
-        self._ref_pos_publisher = self.create_publisher(PoseStamped, '/feely_drone/ref_pose', qos_profile_sensor_data)
-        self._ref_joint_state_publisher = self.create_publisher(JointState, '/feely_drone/ref_joint_state', qos_profile_sensor_data)
-        self._bin_touch_data_publisher = self.create_publisher(JointState, '/feely_drone/bin_touch_state', qos_profile_sensor_data)
-        self._sm_State_publisher = self.create_publisher(StateMachineState, '/feely_drone/state_machine_state', qos_profile_sensor_data)
-        # Subscribers
-        self._touch_data_subscriber = self.create_subscription(TouchData, '/feely_drone/touch_data', self.touch_data_callback, qos_profile_sensor_data)
+        self._ref_pos_publisher = self.create_publisher(PoseStamped, '/feely_drone/in/ref_pose', qos_profile_sensor_data)
+        self._ref_joint_state_publisher = self.create_publisher(JointState, '/feely_drone/in/ref_joint_state', qos_profile_sensor_data)
+        self._bin_touch_data_publisher = self.create_publisher(JointState, '/feely_drone/out/bin_touch_state', qos_profile_sensor_data)
+        self._sm_State_publisher = self.create_publisher(StateMachineState, '/feely_drone/out/state_machine_state', qos_profile_sensor_data)
+        self._contact_marker_publisher = self.create_publisher(MarkerArray, '/feely_drone/out/contact_marker', qos_profile_sensor_data)
 
+        # Subscribers
+        self._touch_data_subscriber = self.create_subscription(TouchData, '/feely_drone/out/touch_data', self.touch_data_callback, qos_profile_sensor_data)
+        self._odometry_subscriber = self.create_subscription(PoseStamped, '/feely_drone/out/pose', self.odometry_data_callback, qos_profile_sensor_data)
+
+        # Init Timers
         self.timer = self.create_timer(1.0 / self.frequency, self.timer_callback)
 
+        # Init static offsets for the arms
+        p0 = np.array([[-0.125, 0.125, 0.0],
+                       [ 0.0,  -0.125, 0.0],
+                       [ 0.125, 0.125, 0.0]])
+        rot0 = np.array([
+            R.from_euler('xyz', [-np.deg2rad(90), 0.0, 0.0]).as_matrix(),
+            R.from_euler('xyz', [-np.deg2rad(90), 0.0, -np.deg2rad(180)]).as_matrix(),
+            R.from_euler('xyz', [-np.deg2rad(90), 0.0, 0.0]).as_matrix(),
+        ]).reshape(3, 3, 3)  
+
+
         # Init the state machine
-        self.sm = StateMachine(dt=1.0 / self.frequency,        # Delta T
-                               m_arm=np.ones(3),               # Mass of the Arm
-                               l_arm=np.ones(3),               # Length of the Arm
-                               p0=np.zeros([3, 3]),            # Offset Position of Arms
-                               rot0=np.zeros([3, 3, 3]),       # Offset Rotation of Arms
-                               K=np.diag(np.ones(3)),          # Stiffness Matrix of the arm
-                               A=np.ones(3),                   # Actuation map
-                               q0=np.deg2rad(75) * np.ones(3), # Neutral joint states
-                               g=np.array([0, 0, -9.81]),      # Gravity Vector
+        self.sm = StateMachine(dt=1.0 / self.frequency,            # Delta T
+                               m_arm=np.ones(3),                   # Mass of the Arm
+                               l_arm=np.array([0.15, 0.25, 0.25]), # Length of the Arm
+                               p0=p0,                              # Offset Position of Arms
+                               rot0=rot0,                          # Offset Rotation of Arms
+                               K=np.diag(100*np.ones(3)),          # Stiffness Matrix of the arm
+                               A=-120 * np.ones(3),                # Actuation map
+                               q0=np.deg2rad(75) * np.ones(3),     # Neutral joint states
+                               g=np.array([0, 0, -9.81]),          # Gravity Vector
                                target_pos_estimate=np.array([0, 0, 1.5]),
                                target_yaw_estimate=np.zeros([1]))
         
         # Init the binary touch state
         self._bin_touch_state = np.zeros(9, dtype=bool)
+
+        # Pose
+        self._position = np.zeros(3, dtype=float)
+        self._yaw = 0.0
 
     def timer_callback(self):
         # Get the reference pose and joint state messages
@@ -61,10 +82,37 @@ class StateMachineNode(Node):
         sm_state_msg = StateMachineState()
         sm_state_msg.state = self.sm.state.value
         sm_state_msg.header.stamp = pose_msg.header.stamp
+        
         # Publish the reference pose and joint state messages
         self._ref_pos_publisher.publish(pose_msg)
         self._ref_joint_state_publisher.publish(joint_state_msg)
         self._sm_State_publisher.publish(sm_state_msg)
+
+        # Publish the contact marker for visualization
+        contact_locs = self.sm.contact_locs
+        marker_array = MarkerArray()
+        for i in range(3):
+            for j in range(3):
+                marker = Marker()
+                marker.header.stamp = pose_msg.header.stamp
+                marker.header.frame_id = 'world'
+                marker.id = i + j * 3 + 1  # Unique ID for each marker
+                marker.type = Marker.SPHERE
+                marker.action = Marker.ADD
+                marker.pose.position.x = contact_locs[i, j, 0]
+                marker.pose.position.y = contact_locs[i, j, 1]
+                marker.pose.position.z = contact_locs[i, j, 2]
+                marker.scale.x = 0.05
+                marker.scale.y = 0.05
+                marker.scale.z = 0.05
+                marker.color.a = 1.0
+                marker.color.r = 1.0 if self._bin_touch_state[i] else 0.0
+                marker.color.g = 0.0
+                marker.color.b = 1.0 if not self._bin_touch_state[i] else 0.0
+                marker.lifetime = rclpy.duration.Duration(seconds=1.0 / self.frequency).to_msg()
+                
+                marker_array.markers.append(marker)
+        self._contact_marker_publisher.publish(marker_array)
 
     def get_references(self):
         # Get Timestamp
@@ -112,6 +160,13 @@ class StateMachineNode(Node):
         self.touch_data_deque.popleft()
         self.touch_data_deque.append(TouchData.raw_data)
         self.sm.update_tactile_info_sw()
+
+    def odometry_data_callback(self, msg: PoseStamped):
+        self._position = msg.pose.position
+        self._yaw = np.atan2(
+            2.0 * (msg.pose.orientation.w *msg.pose.orientation.z + msg.pose.orientation.x * msg.pose.orientation.y),
+            msg.pose.orientation.w**2 + msg.pose.orientation.x**2 - msg.pose.orientation.y**2 - msg.pose.orientation.z**2
+        )
 
 def main(args=None):
     rclpy.init(args=args)
