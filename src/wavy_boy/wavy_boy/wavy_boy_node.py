ae19837d569094305a964308f82098ecee09a147
antbre <a.bredenbeck@tudelft.nl>
2025-06-16 12:20:32 +0200

    do any() along correct axis

--- src/wavy_boy/wavy_boy/wavy_boy_node.py
+++ src/wavy_boy/wavy_boy/wavy_boy_node.py
@@ -64,7 +64,7 @@ class WavyBoyNode(Node):
         joint_state_msg.header.stamp = self.get_clock().now().to_msg()
         #joint_state_msg.name = ['joint_1', 'joint_2', 'joint_3']
 
-        nocontact = ~self.bin_touch_data.any(axis=1)
+        nocontact = ~self.bin_touch_data.any(axis=0)
         
         self._alpha = np.clip(
             self._alpha + self.ALPHA_RATE * (nocontact.astype(float)) 
@@ -86,7 +86,7 @@ class WavyBoyNode(Node):
         self.bin_touch_data = (np.mean(np.abs(
              np.array(self.touch_data_deque)
            - np.array(self.touch_data_baseline_deque)
-           ), axis=1) > self.TOUCH_THRESHOLD)[:9].reshape([3, 3]).transpose()
+           ), axis=0) > self.TOUCH_THRESHOLD)[:9].reshape([3, 3]).transpose()
 
 
 def main(args=None):
